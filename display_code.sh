1ee5e705632c4875b81ceede565a972c124e8525
Moshe Schorr <mosheraphael@gmail.com>
2019-11-17 23:56:24 +0200

    changed display convenience script

--- display_code.sh
+++ display_code.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-echo "AArch Input"
-aarch64-linux-gnu-gcc -S -o - $1
+#echo "AArch Input"
+#aarch64-linux-gnu-gcc -S -o - $1
 
-echo ""
-echo "Transpiled"
+#echo ""
+#echo "Transpiled"
 aarch64-linux-gnu-gcc -S -o - $1 | python3 arm2riscv.py
