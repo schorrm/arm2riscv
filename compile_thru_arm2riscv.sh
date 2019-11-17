657e8d7001b6a09cb157e09cf2f541aea31aaaf1
Moshe Schorr <mosheraphael@gmail.com>
2019-11-17 17:29:46 +0200

    added shell scripts for checking convenience

---
+++ compile_thru_arm2riscv.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+aarch64-linux-gnu-gcc -S -o - $1 | python3 arm2riscv.py | riscv64-linux-gnu-gcc -static -x assembler -
---
+++ display_code.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo "AArch Input"
+aarch64-linux-gnu-gcc -S -o - $1
+
+echo ""
+echo "Transpiled"
+aarch64-linux-gnu-gcc -S -o - $1 | python3 arm2riscv.py
