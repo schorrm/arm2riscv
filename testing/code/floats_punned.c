ae3a591ac2ec856993dfe7a7896573aa82599a58
Moshe Schorr <mosheraphael@gmail.com>
2019-12-18 01:04:32 +0200

    added float testing, uses type punning to avoid floating point variadics

---
+++ testing/code/floats_punned.c
@@ -0,0 +1,57 @@
+// Test file for a full range of **integer** math instructions
+#include <stdio.h>
+
+typedef union {
+    unsigned long int ui;
+    double d;
+} flpun;
+
+int main() {
+    flpun x, y, z, r;
+    x.d = 12378.10844990232;
+    y.d = 1233.99245;
+    z.d = 4113.2329104;
+
+    printf("Floating ops\n");
+    r.d = x.d + y.d;
+    printf("Addition: %lu\n", r.ui);
+    r.d = x.d - y.d;
+    printf("Subtraction: %lu\n", r.ui);
+    r.d = x.d * y.d;
+    printf("Multiplication: %lu\n", r.ui);
+    r.d = x.d / y.d;
+    printf("Divistion: %lu\n", r.ui);
+        // Target the advanced ops:
+
+    asm("fmadd %d[r], %d[x], %d[y], %d[z]"
+        : [r] "=w"(r)
+        : [x] "w"(x), [y] "w"(y), [z] "w"(z));
+    printf("Multiply-Add: %lu\n" , r.ui);
+
+    asm("fmsub %d[r], %d[x], %d[y], %d[z]"
+        : [r] "=w"(r)
+        : [x] "w"(x), [y] "w"(y), [z] "w"(z));
+    printf("Multiply-Subtract: %lu\n" , r.ui);
+
+    asm("fnmsub %d[r], %d[x], %d[y], %d[z]"
+        : [r] "=w"(r)
+        : [x] "w"(x), [y] "w"(y), [z] "w"(z));
+    printf("Negative Multiply-Subtract: %lu\n" , r.ui);
+
+    asm("fnmadd %d[r], %d[x], %d[y], %d[z]"
+        : [r] "=w"(r)
+        : [x] "w"(x), [y] "w"(y), [z] "w"(z));
+    printf("Negative Multiply-Add: %lu\n" , r.ui);
+
+    asm("fneg %d[r], %d[x]"
+        : [r] "=w"(r)
+        : [x] "w"(x));
+    printf("FNegate: %lu\n" , r.ui);
+
+    asm("fsqrt %d[r], %d[x]"
+        : [r] "=w"(r)
+        : [x] "w"(x));
+    printf("Square Root: %lu \n" , r.ui);
+
+    return 0;
+}
