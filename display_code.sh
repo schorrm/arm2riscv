fac11b5a256be0f14d931ac50403d3c237d4bb6f
Moshe Schorr <mosheraphael@gmail.com>
2020-03-09 16:11:10 +0200

    tweaked display util

--- display_code.sh
+++ display_code.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
-#echo "AArch Input"
-#aarch64-linux-gnu-gcc -S -o - $1
 
-#echo ""
-#echo "Transpiled"
-aarch64-linux-gnu-gcc -march=armv8.3-a -S -o - $1 | python3 arm2riscv.py $2
+aarch64-linux-gnu-gcc -march=armv8.3-a -S -o - $1 | python3 arm2riscv.py "${@:2}"
